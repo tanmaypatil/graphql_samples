017e7dbab207dfe75cf60464b7b5d1a82a294816
tanmaypatil <tany.patil77@gmail.com>
2019-11-11 10:23:35 +0530

    example for merging two arrays attribute wise - vertical joining

---
+++ merge_arrays_lodash.js
@@ -0,0 +1,4 @@
+var arr1 = [ { f1 : "tanmay" , f2 : "patil"} , { f1 : "arti" , f2 : "patil"}];
+var arr2 = [ { f3: "sudhakar"} , { f3: "ravindra"}];
+var final = _.merge(arr1,arr2);
+console.log(final)
